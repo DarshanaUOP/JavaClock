04ccb01210e9863abbc03db57b80f5b317693795
DarshanaUOP <46531949+DarshanaUOP@users.noreply.github.com>
2020-04-04 23:12:07 +0530

    Update Clock.java

--- src/Clock.java
+++ src/Clock.java
@@ -5,18 +5,8 @@ import java.util.Date;
 /**
  * Created by acer on 18-Dec-17.
  */
-public class Clock {
+public class Clock{
     public static void main(String[] args) {
-        /*
-        while(true){
-            //System.out.println(new SimpleDateFormat("hh:mm:ss , dd,MMMMMM,yyyy").format(new Date()));
-            System.out.println(new SimpleDateFormat("ss").format(new Date()));
-
-            try{Thread.sleep(500);}catch (Exception e){}
-        }
-        */
-
-        //System.out.println(new SimpleDateFormat("hh:mm:ss a, dd,MMMMMM,yyyy").format(new Date()));
 
         clockGUI clock = new clockGUI();
         clock.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
